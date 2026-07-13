fd6bd3e013b5c2df4a85b04737fab6999265038c
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-13 23:41:55 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.07%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0054-maximum-subarray/0054-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
