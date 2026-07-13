030c189ea84f32aaf94da9918a73834b64236ee9
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-13 23:43:40 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.62%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0056-jump-game/0056-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxReach)
+                return false;
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (maxReach >= nums.size() - 1)
+                return true;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
