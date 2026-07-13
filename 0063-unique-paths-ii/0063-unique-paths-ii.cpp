cfcf1556a80d964357df8a3f2f3e3b29e47f7247
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-13 23:51:53 +0530

    Time: 1 ms (15.84%), Space: 11.6 MB (79.09%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<int> dp(n, 0);
+
+        dp[0] = (obstacleGrid[0][0] == 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                }
+                else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
