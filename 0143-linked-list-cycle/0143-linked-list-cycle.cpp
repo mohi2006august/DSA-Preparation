f8a952ccd535d49ac667a7b551ee287eee3a5024
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-14 00:41:54 +0530

    Time: 12 ms (43.24%), Space: 11.8 MB (81.57%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0143-linked-list-cycle/0143-linked-list-cycle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
