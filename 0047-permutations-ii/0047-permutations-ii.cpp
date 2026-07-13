ad94f9469229ed59b5e103eedb069d133153d3d7
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-13 22:45:58 +0530

    Time: 0 ms (100%), Space: 11.9 MB (61.55%) - LeetHub - Time: O, Space: O(?)

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+    vector<bool> visited;
+
+    void backtrack(vector<int>& nums) {
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (visited[i])
+                continue;
+
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
+                continue;
+
+            visited[i] = true;
+            path.push_back(nums[i]);
+
+            backtrack(nums);
+
+            path.pop_back();
+            visited[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        visited.resize(nums.size(), false);
+
+        backtrack(nums);
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
