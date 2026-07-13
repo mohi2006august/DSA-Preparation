28c32ee7f4eec41baf26d31563cda031ea4d6907
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-14 00:29:35 +0530

    Time: 7 ms (98.36%), Space: 81.5 MB (62.27%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && currHeight < heights[st.top()]) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
