c4938001d6cc098671d717397262278637e85207
MOHAMMED MOHIUDDIN <nizamuddinnumaan822@gmail.com>
2026-07-14 00:46:42 +0530

    Time: 15 ms (85.3%), Space: 107.2 MB (47.66%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st; // stores indices
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int idx = st.top();
+                st.pop();
+                ans[idx] = i - idx;
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
